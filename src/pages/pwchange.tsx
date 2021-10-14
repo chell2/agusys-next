9e3493efd653c24f32a03e9b1e3106b7a8980a30
chell2 <rei1259@gmail.com>
2021-10-14 16:41:07 +0900

    delete button

--- src/pages/pwchange.tsx
+++ src/pages/pwchange.tsx
@@ -4,6 +4,23 @@ const PwChange = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
+            <div className="text-right">
+              <button className="btn btn-outline btn-square btn-xs text-neutral hover:bg-neutral">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-4 h-4 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              </button>
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">新しいパスワード</span>
@@ -24,7 +41,7 @@ const PwChange = () => {
                 className="input input-bordered focus:ring-2 focus:ring-primary"
               />
             </div>
-            <div className="form-control mt-6">
+            <div className="form-control mt-8">
               <div className="btn-group">
                 <button className="btn btn-outline btn-primary w-1/2">Cancell</button>
                 <button className="btn btn-primary w-1/2">OK</button>
